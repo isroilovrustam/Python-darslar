0219369db8935740e9e06052852f7503ce3617b0
Rustam Isroilov <105547303+isroilovrustam@users.noreply.github.com>
2022-09-27 20:02:26 +0500

    1-dars-python qo`shildi
    1-dars-python qo`shildi

---
+++ 1-dars-python.py
@@ -0,0 +1,27 @@
+# Python Foundation_
+
+# 1- dars Print and Arifmetika
+
+print("Hello World")
+print('Assalom alaykum')
+print('Men "Python" ni boshladim')
+
+""" 3 talik qo`shtirnoq bilan keyingi qatorga o`tish """
+print("""Hello World
+Salom Dunyo""")
+
+""" \n vazifasi qatorga ajratish """
+print("Hello World \nSalom Dunyo")
+
+""" Qoidalaga asosan hisoblidi """
+print(2 + 3 * 4)
+
+""" Kasir qismini hisoblaydi """
+print(19 / 5)
+
+""" Butun qismini hisoblaydi """
+print(10 // 3)
+
+""" Soni darajaga ko`tarish """
+print(2 ** 4)
+print("4 ning kvadrati", 4 ** 2, "ga teng")
