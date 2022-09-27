9694ca79408206fea81a6f5cacda06137d767281
Rustam Isroilov <105547303+isroilovrustam@users.noreply.github.com>
2022-09-27 22:56:54 +0500

    yangi fayl qo`shildi
    2-dars qoshildi

---
+++ 2-dars-python.py
@@ -0,0 +1,28 @@
+# 2 - dars O`zgaruvchilar (Variables)
+
+
+# ism = "Abdulloh"
+# yosh = 12
+# print(ism)
+# print(yosh)
+
+# ism = "Abdulloh"
+# print(ism)
+# ism = "Muhammad"
+# print(ism)
+
+""" O'zgaruvchilarga nom berishda quyidagi qoidalarga amal qiling:
+   O'zgaruvchi nomi harf yoki pastki chiziq (_) bilan boshlanishi kerak
+   O'zgaruvchi nomi raqam bilan boshlanishi mumkin emas
+   O'zgaruvchi nomida faqatgina lotin alifbosi harflari (A-z), raqamlar (0-9) va pastki chiziq (_) qatnashishi mumkin
+   O'zgaruvchi nomida bo'shliq (пробел) bo'lishi mumkin emas
+   O'zgaruvchi nomida katta-kichik harflar turlicha talqin qilinadi (ism, ISM, va Ism uchta turli o'zgaruvchi) """
+
+""" Qo'shimcha qoida sifatida:
+   O'zgaruvchi nomini kichik harflar bilan yozing.
+   O'zgaruvchi nomida 2 va undan ortiq so'z qatnashsa ularning orasini pastki chiziq (_) bilan ajrating (ism_sharif="Anvar Narzullaev")
+   O'zgaruvchiga tushunarli nom bering (y=20 emas yosh=20, d="Korea" emas davlat = "Korea" va hokazo)
+   Shuningdek o'zgaruvchilarga Pythonda ishlatiladigan funktsiyalar va maxsus kalit so'zlarning (keywords) nomini bermang.
+   Kalit so'zlar ro'yhatini ko'rish uchun Spyder konsolida avval help() deb yozing va Enter tugmasini bosing.
+   Keyin esa keywords deb kiritib, yana Enter bosing. Marhamat, ekraningizda Pythondagi maxsus kalit so'zlar ro'yhatini ko'ryapsiz:"""
+
